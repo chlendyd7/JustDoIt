158b02d8be3eabef759014e196c29e75589332b8
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2022-02-23 12:02:25 +0900

    카카오 지도api

---
+++ kakao_map_api/kakao_map_api/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kakao_map_api
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser3_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Locale> locales = KakaoAPI.Search(textBox1.Text);
+            listBox1.Items.Clear();
+            foreach(Locale item in locales)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1.PerformClick(); //button1 누름
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+            Locale ml = listBox1.SelectedItem as Locale;
+            object[] position = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser3.Document;
+            hdoc.InvokeScript("setCenter", position);
+
+        }
+    }
+}
