362e970631350018da973d25ace4edf3d5e267a6
Choi Du Young <chlendyd7@naver.com>
2022-03-09 23:22:40 +0900

    Structure

---
+++ Java_structure/src/list/linkedlist/implementation/ArrayList_practice.java
@@ -0,0 +1,48 @@
+package list.linkedlist.implementation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList_practice {
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		System.out.println("add(값)");
+		System.out.println(numbers);
+		
+		numbers.add(1,50);
+		System.out.println("\nadd(인덱스,값)");
+		System.out.println(numbers);
+		
+		numbers.remove(1);
+		System.out.println("\nadd(인덱스,값)");
+		System.out.println(numbers);
+		
+		System.out.println("\nget(인덱스)");
+		System.out.println(numbers.get(2));
+		
+		System.out.println("\nindexOf()");
+        System.out.println(numbers.indexOf(30));
+
+		
+		Iterator it = numbers.iterator();
+		while(it.hasNext()) {
+			int value = (int)it.next();
+		}
+		
+		for(int value1:numbers) {
+			System.out.println(value1);
+			
+		}
+		System.out.println("\nfor");	
+		for(int i =0; i<numbers.size();i++) {
+			System.out.println(numbers.get(i));
+		}
+		
+	}
+
+}
