c3cf02caa62d876e7f886bda4a187cfe155c6e23
Choi Du Young <chlendyd7@naver.com>
2022-03-11 18:49:36 +0900

    1

---
+++ Java_structure/src/list/doublylinkedlist/implementation/copy/Main.java
@@ -0,0 +1,13 @@
+package list.doublylinkedlist.implementation.copy;
+ 
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList numbers = new DoublyLinkedList();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.add(1, 15);
+    }
+        
+ 
+}
